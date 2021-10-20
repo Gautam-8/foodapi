5c2827e4f09e66608dd9a116abaad729cc039324
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2021-10-20 20:34:19 +0530

    Create navbar.js

---
+++ components/navbar.js
@@ -0,0 +1,16 @@
+function navbar(){
+
+    return `<div id="navbar">
+    <h3><a href="home.html">Home</a></h3>
+
+    <div id="options">
+      <h3><a href="latest.html">latest</a></h3>
+      <h3><a href="recipe.html">recipe of day</a></h3>
+    
+    </div>
+  </div>`
+
+
+}
+
+export default navbar

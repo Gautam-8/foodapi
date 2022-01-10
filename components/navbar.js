0e8bb3d43ef0ecbb25b3855cca75f1ad12cc1521
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-10 22:04:40 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 function navbar(){
 
     return `<div id="navbar">
-    <h3><a href="home.html">Home</a></h3>
+    <h3><a href="index.html">Home</a></h3>
 
     <div id="options">
       <h3><a href="latest.html">latest</a></h3>
